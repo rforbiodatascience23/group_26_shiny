0f3736ba055aa57fcf74e6f09fa0d128da19ddfa
Freies <felix.freiesleben@gmail.com>
2023-10-31 09:26:53 +0100

    Changed name of package and authors information

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ##
 golem::fill_desc(
   pkg_name = "CDVisual", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
+  pkg_title = "Visualisation of the Central Dogma", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  author_first_name = "Emilie", # Your First Name
+  author_last_name = "Munk", # Your Last Name
+  author_email = "s203549@dtu.dk", # Your Email
   repo_url = NULL, # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
